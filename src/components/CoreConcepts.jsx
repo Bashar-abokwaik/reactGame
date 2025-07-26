71bf645d6951bd22e85f8d862565a12746d1c05e
Bashar-abokwaik <basharabokaik@gmail.com>
2025-07-26 14:27:04 +0300

    update

--- src/components/CoreConcepts.jsx
+++
@@ -1,9 +0,0 @@
-export default function CoreConcepts(prop) {
-    return (
-        <li>
-            <img src={prop.image} alt={prop.title} />
-            <h3>{prop.title}</h3>
-            <p>{prop.description}</p>
-        </li>
-    )
-}
